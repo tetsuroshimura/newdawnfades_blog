547b960c1414e409361eb67f3d8976b101254cc1
Tetsuro Shimura <tetsuroshimura@shimuratetsuro.local>
2022-07-05 15:57:52 +0900

    indexのスタイルを調整

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -11,19 +11,17 @@ export function ArticleCard({ article }: { article: Content & Article }) {
       <Link href={`/article/${article.slug}`}>
         <a href="#" className={styles.Article_Link}>
           <div className={styles.Article_Inner}>
-            <h2 className={styles.Article_Title}>{article.title}</h2>
-            <div className={styles.Article_Content}>
-              <div className={styles.Article_Data}>
-                <div className={styles.Article_DataText}>
-                  <time
-                    dateTime={formatDate(article._sys.createdAt)}
-                    className={styles.Article_Date}
-                  >
-                    {formatDate(article._sys.createdAt)}
-                  </time>
-                </div>
+            <div className={styles.Article_Data}>
+              <div className={styles.Article_DataText}>
+                <time
+                  dateTime={formatDate(article._sys.createdAt)}
+                  className={styles.Article_Date}
+                >
+                  {formatDate(article._sys.createdAt)}
+                </time>
               </div>
             </div>
+            <h2 className={styles.Article_Title}>{article.title}</h2>
           </div>
         </a>
       </Link>
