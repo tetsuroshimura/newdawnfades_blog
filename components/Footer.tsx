b127adf32763294b184519faaad86f1c14161487
Tetsuro Shimura <tetsuroshimura@shimuratetsuro.local>
2022-07-05 15:04:25 +0900

    footerにテキストを追加

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,6 +6,16 @@ export function Footer({ app }: { app: AppMeta }) {
   return (
     <footer className={styles.Footer}>
       <div className={styles.Footer_Inner}>
+        <div className={styles.Footer_Text}>
+          <ul className={styles.Footer_Text_Block}>
+            <li className={styles.Footer_Text_List}>Typeface : TWK ghost, Noto Serif JP</li>
+            <li className={styles.Footer_Text_List}>Framework : Next.js</li>
+            <li className={styles.Footer_Text_List}>CMS : Newt</li>
+            <li className={styles.Footer_Text_List}>Server : Vercel</li>
+            <li className={styles.Footer_Text_List}>Design : Shimura Tetsuro</li>
+            <li className={styles.Footer_Text_List}>URL : https://www.newdawnfades.today/</li>
+          </ul>
+        </div>
         <Link href="/">
           <a href="#" className={styles.SiteName}>
             {app.icon?.type === "emoji" && (
