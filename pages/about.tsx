8fb563bad58cc64eb1313e8ec406f23436b60366
Tetsuro Shimura <tetsuroshimura@shimuratetsuro.local>
2022-07-15 17:49:38 +0900

    aboutページのリンクを追加

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "../styles/About.module.css";
+
+export function About(){
+  return (
+    <div className={styles.Wrapper}>
+      About Pages
+    </div>
+  );
+}
